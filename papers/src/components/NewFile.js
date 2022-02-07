aeccb75d37115e221e53523f1ac4ec58a0561ad1
Umesh <umeshsikka0123@gmail.com>
2022-02-07 20:03:46 +0530

    popup added

--- papers/src/components/NewFile.js
+++ papers/src/components/NewFile.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 import AddIcon from '@mui/icons-material/Add';
 import '../assets/stylesheets/NewFile.css'
 
-function NewFile() {
+export default function NewFile(){
+  const [newfile, setNewFile] = useState(false);
+
+  const toggleNewFile = () => {
+    setNewFile(!newfile);
+  };
+
+
   return (
-      <div className='newFile'>
-        <div className='newFile__container'>
-          <AddIcon />
-          <p>New</p>
+    <>
+      <button onClick={toggleNewFile} className="newFile">
+        New
+      </button>
+
+      {newfile && (
+        <div className="newFile">
+          <AddIcon/>  
+          
+          <div className="newFile__container">
+            <div onClick={toggleNewFile} className="overlay"></div>
+            <p>
+              New
+            </p>
+          </div>
         </div>
-      </div>
-  )
+      )}
+      
+      
+    </>  
+  );
 }
 
-export default NewFile;
+
